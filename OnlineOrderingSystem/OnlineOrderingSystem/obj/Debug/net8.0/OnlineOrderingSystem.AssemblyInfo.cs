a3098883c1831f2e7eeb463f3b04ca2897b039ad
MaenAlNjar <maen.njarr@gmail.com>
2024-08-12 23:25:52 +0300

    23th

--- OnlineOrderingSystem/OnlineOrderingSystem/obj/Debug/net8.0/OnlineOrderingSystem.AssemblyInfo.cs
+++ OnlineOrderingSystem/OnlineOrderingSystem/obj/Debug/net8.0/OnlineOrderingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineOrderingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d80120c27dc21d70176e7b029cd64bc0e71d62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f47821ca768a0946ef97b525053eafdf4b7154")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineOrderingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineOrderingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
